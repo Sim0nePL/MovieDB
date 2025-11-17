f309b389e77f781c892dc7f70c1e361d3f0417b7
BuildTools <unconfigured@null.spigotmc.org>
2025-11-17 22:38:01 +0100

    a chuj go wie

---
+++ src/lib/components/HeaderRight.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    import TextInput from '$lib/components/TextInput.svelte';
+    import Button from '$lib/components/Button.svelte';
+
+    let searchValue: string = $state('');
+</script>
+
+<div class="header__right">
+    <h3>Odkryj świat <span>filmów</span>!</h3>
+    <TextInput id="media_title-input" bind:value={searchValue} placeholder="Black Mirror" />
+    <div class="buttons">
+        <Button>Discover</Button>
+        <Button
+            onclick={() => {
+                if (searchValue == '') return;
+                location.href = ./browse?q=${searchValue};
+            }}>Browse</Button
+        >
+    </div>
+</div>
+
+<style>
+    .header__right {
+        text-align: end;
+    }
+
+    h3 {
+        font-family: var(--font-expletus-sans);
+        font-size: 36px;
+        margin-bottom: 5px;
+    }
+
+    :global(#media_title-input) {
+        width: max(30vw, 360px);
+        margin-bottom: 5px;
+    }
+
+    @media screen and (max-width: 1160px) {
+        .header__right {
+            text-align: center;
+        }
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
    import Header from "$lib/components/Header.svelte";
    import MediaList from "$lib/components/MediaList.svelte";
+   import HeaderRight from "$lib/components/HeaderRight.svelte"
+    import Button from "$lib/components/Button.svelte";
 </script>
 
-<Header/>
+<Header>
+    <HeaderRight/>
+</Header>
 <main>
 	<MediaList mediaType="movie"></MediaList>
 	<MediaList mediaType="tvshow"></MediaList>
