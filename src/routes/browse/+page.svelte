af82b5e7fa98410f927949b90b83ee3687eef50e
Sim0nePL <sim0nepl.krzyk.public@gmail.com>
2025-11-17 21:07:20 +0100

    Add Browse page

---
+++ src/routes/browse/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Header from '$lib/components/Header.svelte';
+	import Button from '$lib/components/Button.svelte';
+	import TextInput from '$lib/components/TextInput.svelte';
+
+	import { onMount } from 'svelte';
+
+	let searchValue = $state('');
+
+	onMount(() => {
+		const params = new URLSearchParams(location.search);
+		searchValue = params.get('q') ?? '';
+	});
+</script>
+
+<Header rightSide={false} />
+<Button>{searchValue}</Button>
+<TextInput bind:value={searchValue} placeholder="Witam Wszystkich"/>
