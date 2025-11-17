442f88dfb7256780195cf7be0a22583fb62ae352
BuildTools <unconfigured@null.spigotmc.org>
2025-11-17 22:31:17 +0100

    chuj do dupy

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+   import Header from "$lib/components/Header.svelte";
+   import MediaList from "$lib/components/MediaList.svelte";
+</script>
+
+<Header/>
+<main>
+	<MediaList mediaType="movie"></MediaList>
+	<MediaList mediaType="tvshow"></MediaList>
+</main>
